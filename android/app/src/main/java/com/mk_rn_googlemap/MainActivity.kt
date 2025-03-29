97c405f13a33d040dd722b63743342c30779e151
KUNAAL MANGAL <ksmangal1998@gmail.com>
2025-03-29 23:23:46 +0530

    - Upgrade project to the latest react-native version (0.75.4 → 0.78.1).

--- android/app/src/main/java/com/mk_rn_googlemap/MainActivity.kt
+++ android/app/src/main/java/com/mk_rn_googlemap/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.demoapp
+package com.mk_rn_googlemap
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "demoapp"
+  override fun getMainComponentName(): String = "MK_RN_GoogleMap"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
